053513578461b0be24a223465adcde488e103f6a
Pavel Tarakanov <pavel191094@icloud.com>
2025-07-05 07:26:49 +0300

    add mixin for relationship or foreign key

--- app/models/profile.py
+++ app/models/profile.py
@@ -1,17 +1,14 @@
-from typing import TYPE_CHECKING
-
-from sqlalchemy import String, ForeignKey
+from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
+from .mixins import UserRelationMixin
 
 from .base import Base
 
-if TYPE_CHECKING:
-    from .user import User
 
+class Profile(UserRelationMixin, Base):
+    _user_id_unique = True
+    _user_back_populates = "profile"
 
-class Profile(Base):
     first_name: Mapped[str | None] = mapped_column(String(45))
     last_name: Mapped[str | None] = mapped_column(String(45))
     bio: Mapped[str | None]
-
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), unique=True)
--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,15 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+
+if TYPE_CHECKING:
+    from .profile import Profile
 
 
 class User(Base):
     username: Mapped[str] = mapped_column(String(32), unique=True)
+
+    profile: Mapped["Profile"] = relationship(back_populates="user")
